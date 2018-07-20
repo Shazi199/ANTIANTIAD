2c72ebc9a4ba2e9dd19824c18af4a0f16fcc426f
Shazi199 <shazi199@gmail.com>
2018-07-20 12:09:59 +0800

    Create ANTIANTIAD.user.js

---
+++ ANTIANTIAD.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         ANTI ANTIAD
+// @namespace    https://github.com/Shazi199/
+// @version      0.0.3
+// @description  Unlock MHXG WIKI's right click and select.
+// @author       Shazi199
+// @license      MIT License
+// @match        http://www.ruanyifeng.com/blog/*
+// @grant        none
+// @require      http://code.jquery.com/jquery-1.11.0.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    if(window.checker){
+        window.checker=function(){};
+    }
+})();
